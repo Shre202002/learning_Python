c24f4027a67332985f8e81843e88abc8a208e6e1
Sriyansh Gupta <sriyanshgupta24@gmail.com>
2024-09-19 20:32:11 +0530

    File IO Pratice Questions

--- pratice/File_IO/five/find_line.py
+++ pratice/File_IO/five/find_line.py
@@ -1,1 +1,8 @@
-with open(r"")
+#To find Out the line over which the data/Text  is present 
+
+with open(r"D:\learning_Python\pratice\File_IO\five\data.txt") as f:
+    p = f.readlines()
+    print(p, type(p))
+    line = 1
+    if ("python" in f.readline()):
+        print(f"yes {word} is present")
