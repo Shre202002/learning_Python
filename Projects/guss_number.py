f967836907f4894d0aa79c985f94438785ccaa31
Sriyansh Gupta <sriyanshgupta24@gmail.com>
2024-10-21 09:32:10 +0530

    Learning The Advance Concepts In Python

--- Projects/guss_number.py
+++ Projects/guss_number.py
@@ -3,27 +3,32 @@ import random
 num = random.randint(1,100)
 a = -1
 mov = 0
+choosen= []
 
 print(num)
 while( a != num):
     a = int(input("Guess a number: "))
 
     if(a == num):
-        print(f"Yes You had guess the correct number the number is {num} \nNumber of moves are {mov}")
+        print(f"Yes You had guess the correct number the number is {num} \nNumber of moves are {mov} \nNumbers You had choosen are {choosen}")
     else:
         if (a > num ):
 
             if(a-num <=10):
                 print(" You are Close to number")
                 mov +=1
+                choosen.append(a)
             else:
                 print(" Chosen Smaller Number ")
-            mov +=1
+                choosen.append(a)
+                mov +=1
         else:
             if(num-a <= 10):
                 print(" You are Close to number")
+                choosen.append(a)
                 mov +=1
             else:
                 print(" Chosen Grater Number ")
-            mov +=1
+                choosen.append(a)
+                mov +=1
         
