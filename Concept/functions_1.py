65f9291737c1e6cdf23cbbd0c2f73bd09e4c58f2
Sriyansh Gupta <sriyanshgupta24@gmail.com>
2024-10-22 11:51:21 +0530

    Leared Opps Concepts and created projects of that

---
+++ Concept/functions_1.py
@@ -0,0 +1,12 @@
+# factorial of number using the function 
+
+def fact(s):
+    # print(s)
+    fact =1
+    for i in range (1,s+1):
+        fact = fact * i
+    return fact
+
+i = int(input("Enter Number for factorial : "))
+f = fact(i)
+print(f"factorial of number is : {f}")
